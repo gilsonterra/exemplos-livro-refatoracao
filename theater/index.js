79cb1df7e6156deb4fafd582cc6328bd7017118e
Gilson Terra <gilsonterra@gmail.com>
2020-08-30 17:24:48 -0300

    remove parameters

--- theater/index.js
+++ theater/index.js
@@ -8,7 +8,7 @@ function htmlStatement(invoice, plays){
     return renderHtml(createStatementData(invoice, plays));
 }
 
-function renderHtml(data, plays) {
+function renderHtml(data) {
     let result = `<h1>Statement for ${data.customer}</h1>\n`;
     result += "<table>\n";
     result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
@@ -24,7 +24,7 @@ function renderHtml(data, plays) {
     return result;
 }
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
     let result = `Statement for ${data.customer}\n`;
 
     for (let perf of data.performances) {
