561e3ff109b9516d93b825a00395ee4c46dd2247
Gilson Terra <gilsonterra@gmail.com>
2020-08-30 13:46:48 -0300

    Extract Function

--- theater/index.js
+++ theater/index.js
@@ -1,15 +1,21 @@
-function statement(invoice, plays) {
-    let totalAmount = 0;    
+function statement(invoice, plays) {    
     let result = `Statement for ${invoice.customer}\n`;
 
     for (let perf of invoice.performances) {
-        result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-        totalAmount += amountFor(perf);
+        result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;        
     }
-        
-    result += `Amount owed is ${usd(totalAmount)}\n`;
+    
+    result += `Amount owed is ${usd(totalAmount())}\n`;
     result += `You earned ${totalVolumeCredits()} credits.\n`;
     return result;
+
+    function totalAmount(){
+        let result = 0;
+        for (let perf of invoice.performances) {            
+            result += amountFor(perf);
+        }
+        return result;
+    }
 
     function totalVolumeCredits()
     {
