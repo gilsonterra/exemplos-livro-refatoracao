f3af8e4603fc2cd3a0baa6bf16bc2cb310b0299a
Gilson Terra <gilsonterra@gmail.com>
2020-08-30 13:24:11 -0300

    Extract Function + Replace Temp With Query

--- theater/index.js
+++ theater/index.js
@@ -6,15 +6,20 @@ function statement(invoice, plays) {
         result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
-
-    let volumeCredits = 0;
-    for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
-    }
-
+    let volumeCredits = totalVolumeCredits();
     result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${volumeCredits} credits.\n`;
     return result;
+
+    function totalVolumeCredits()
+    {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += volumeCreditsFor(perf);
+        }
+
+        return result;
+    }
 
     function usd(aNumber){
         return new Intl.NumberFormat('en-US', {
